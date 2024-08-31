8480c09e5a4e466349931e4a95436cd5de35adae
SANTIAGO AROCA <santiagoarocafuentes@gmail.com>
2024-08-31 12:35:44 -0500

    Add project files.

---
+++ BackEndProyecto/Context/dbcontextBank.cs
@@ -0,0 +1,18 @@
+﻿using BackEndProyecto.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BackEndProyecto.Context
+{
+    public class dbcontextBank : DbContext
+    {
+        public dbcontextBank(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<Users> users { get; set; }
+
+// OnModelCreating Tarea
+    }
+}
---
+++ BackEndProyecto/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndProyecto.Models
+{
+    public class Users
+    {
+        [Key]
+        int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
